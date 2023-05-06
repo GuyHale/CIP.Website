0a7cca42eaa9df4d446e10ec4b362f7e14c3b5da
Guy Hale <hale_guy@outlook.com>
2023-05-07 00:41:55 +0100

    Add: Search bar for table and icons to spice up the table page

--- CIP.Website.Data/Models/Cryptocurrency.cs
+++ CIP.Website.Data/Models/Cryptocurrency.cs
@@ -9,10 +9,10 @@ namespace CIP.Website.Data.Models
     public class Cryptocurrency
     {
         public short Rank { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public string? Abbreviation { get; set; }
-        public string? USDValuation { get; set; }
-        public string? MarketCap { get; set; }
+        public string USDValuation { get; set; }
+        public string MarketCap { get; set; }
         public string? Description { get; set; }
     }
 }
